6d230fabe1220c8a9fb29ed367f893f1fc6b8def
iOznny <fo170014@gmail.com>
2021-02-15 20:56:29 -0600

    Avance final seccion

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { Video } from 'src/app/models/youtube.models';
+import { YoutubeService } from 'src/app/services/youtube.service';
+
+// ES6 Modules or TypeScript
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public movies: Video[] = [];
+
+  constructor(private yt: YoutubeService) { }
+
+  /**
+   * El operador de propagación spread operator ( ...element ) permite que una expresión sea expandida 
+   * en situaciones donde se esperan múltiples argumentos 
+   * (llamadas a funciones) o múltiples elementos (arrays literales).
+  */
 
   ngOnInit(): void {
+    this.loadMovies();
+  }
+
+  seeMovie(movie: Video) {
+    console.log(movie);
+
+    Swal.fire({
+      title: movie.title,
+      html: `
+        <iframe width="100%" height="315" src="https://www.youtube.com/embed/${ movie.resourceId.videoId }" 
+        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
+        gyroscope; picture-in-picture" allowfullscreen>
+        </iframe>
+      `
+    });
+  }
+
+  loadMovies() {
+    this.yt.getVideos().subscribe( resp => {
+      this.movies.push(...resp);
+      console.log(this.movies);
+    });
   }
 
 }
--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Youtube } from '../models/youtube.models';
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -6,6 +9,29 @@ import { Injectable } from '@angular/core';
 
 export class YoutubeService {
 
-  constructor() { }
+  private url = 'https://www.googleapis.com/youtube/v3';
+  private apiKey = 'AIzaSyDhw0twGpy-otLH15llM7aq5fdNd9m7_Sw';
+  private playlist = 'UUuaPTYj15JSkETGnEseaFFg';
+  private nextPageToken = '';
+
+  constructor(private http: HttpClient) { 
+  }
+
+  getVideos() {
+    const params = new HttpParams()
+    .set('part', 'snippet')
+    .set('key', this.apiKey)
+    .set('playlistId', this.playlist)
+    .set('maxResults', '10')
+    .set('pageToken', this.nextPageToken)
+
+    return this.http.get<Youtube>(`${ this.url }/playlistItems`, { params }).pipe(
+      map( resp => {
+        this.nextPageToken = resp.nextPageToken;
+        return resp.items
+      }),
+      map( items => items.map( v => v.snippet))
+    );
+  }
   
 }
